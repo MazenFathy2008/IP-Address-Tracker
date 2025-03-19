74b74323bef8266f8051b296a1f8d84499f1bc4c
mazenDev <mazenfathy841@gmail.com>
2025-03-19 04:08:09 +0200

    add buttons to change map theme

--- script/ipTracker.js
+++ script/ipTracker.js
@@ -2,7 +2,6 @@ export async function Iptraker(ip = "") {
   try {
     const ipRespos = await fetch(`http://ipwho.is/${ip}`);
     const ipInfo = await ipRespos.json();
-    console.log(ipInfo)
     return ipInfo;
   } catch (e) {
     console.log(`there is an ${e} error please try later`);
--- script/map.js
+++ script/map.js
@@ -1,10 +1,13 @@
 const map = L.map("map");
-L.tileLayer(
-  "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
-  {
-    attribution: '&copy; <a href="https://www.esri.com/">Esri</a>',
-  }
-).addTo(map);
+let layerInfo = {
+  layer: L.tileLayer(
+    "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
+    {
+      attribution: '&copy; <a href="https://www.esri.com/">Esri</a>',
+    }
+  ).addTo(map),
+  id: "carto",
+};
 const blackIcon = L.icon({
   iconUrl: "../images/icon-location.svg",
   iconSize: [30, 50],
@@ -12,17 +15,38 @@ const blackIcon = L.icon({
 });
 export function setUpMap(latitude, longitude) {
   map.setView([latitude, longitude], 15);
-  // map.eachLayer((layer) => {
-  //   if (layer instanceof L.Marker) {
-  //     map.removeLayer(layer);
-  //   }
-  // });
+  map.eachLayer((layer) => {
+    if (layer instanceof L.Marker) {
+      map.removeLayer(layer);
+    }
+  });
   L.marker([latitude, longitude], {
     icon: blackIcon,
   }).addTo(map);
 }
-// L.tileLayer('https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png', {
-//   attribution: '&copy; <a href="https://carto.com/">CARTO</a>',
-//   subdomains: 'abcd',
-//   maxZoom: 19
-// }).addTo(map);
+document.querySelector(".js-basemaps-button").addEventListener("click", () => {
+  if (layerInfo.id != "basemaps") {
+    map.removeLayer(layerInfo.layer);
+    layerInfo.layer = L.tileLayer(
+      "https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png",
+      {
+        attribution: '&copy; <a href="https://carto.com/">CARTO</a>',
+        subdomains: "abcd",
+        maxZoom: 19,
+      }
+    ).addTo(map);
+    layerInfo.id = "basemaps";
+  }
+});
+document.querySelector(".js-carto-button").addEventListener("click", () => {
+  if (layerInfo.id != "carto") {
+    map.removeLayer(layerInfo.layer);
+    layerInfo.layer = L.tileLayer(
+      "https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}",
+      {
+        attribution: '&copy; <a href="https://www.esri.com/">Esri</a>',
+      }
+    ).addTo(map);
+    layerInfo.id = "carto";
+  }
+});
